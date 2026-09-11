bd5398dc90533040788a06ccd6d4d7b772318335
etherlhamo <hamoshata100@gmail.com>
2026-09-11 18:54:26 +0300

    some features

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 let isGameRunning = false;
 let lives = 10
-let holes = document.querySelectorAll(".hole")
 let score = 0
 let combo = 0
+let combomodifier = 1
 let highscore = 0
 let timeleft = 30
+let spawntimer
 const maxmoles = 6
+// dome selectors
+const holes = document.querySelectorAll(".hole")
+const startbutton = document.querySelector("#start-button")
+const scoreLabel = document.querySelector("#score-tracker")
+const highscoreLabel = document.querySelector("#high-score-tracker")
+const comboLabel = document.querySelector("#combo-tracker")
+
+// buttonm calling different function on a timer
+startbutton.addEventListener("click",()=> {
+    if(!isGameRunning){
+        settime()
+        spawnMole()
+        spawntimer = setInterval(() => {
+            spawnMole()
+        },1000)
+    }  
+})
 
 
 // mole types//
 let moles = [
     {
-        image: "image/first-mole.png",
+        image: "images/first-mole.png",
         type:"normal" ,
         hits: 0,
         maxhits:1,
@@ -29,27 +47,22 @@ let moles = [
         type:"fast",
         hits:0,
         maxhits:1,
-        damage:1
+        damage:1,
+        timebonus:4
 
     },
     {
         image:"images/Pantheon_Render.webp",
-        type:"safe",
-        hits:0
+        type:"trap",
+        hits:0,
+        maxhits:1,
+        damage:1
     }
 ]
 
-
-
-
-
-
-
-
-
 let activeMoles = {}
 
-
+// creating new mole objects from the template//
 function spawnMole (){
     if (Object.keys(activeMoles).length < maxmoles){
         //randomgen//
@@ -66,18 +79,129 @@ function spawnMole (){
         moleImage.style.backgroundImage = `url("${selectedMole.image}")`
         
         moleImage.classList.add("show")
-        let activeMole = {
-        image: selectedMole.image,
-        type: selectedMole.type,
-        hits: 0,
-        maxhits: selectedMole.maxhits,
-        damage: selectedMole.damage
-        }
+    let activeMole = {
+    image: selectedMole.image,
+    type: selectedMole.type,
+    hits: 0,
+    maxhits: selectedMole.maxhits,
+    damage: selectedMole.damage,
+    timebonus: selectedMole.timebonus
+}
+    activeMoles[randomHole] = activeMole
+//exception for the fast mole
+if (selectedMole.type === "fast") {
 
-        activeMoles[randomHole] = activeMole
+activeMole.timer = setTimeout(() => {
+    if (activeMoles[randomHole]) {
+
+        combo = 0
+        comboLabel.textContent = combo
+
+        moleImage.classList.remove("show")
+        delete activeMoles[randomHole]
     }
+}, 850)
+
+} else {
+
+setTimeout(() => {
+    if (activeMoles[randomHole]) {
+        combo = 0
+        comboLabel.textContent = combo
+        moleImage.classList.remove("show")
+        delete activeMoles[randomHole]
+    }
+}, 2000)
+
+}}
+    }
+
+// clicking a hole function//
+
+holes.forEach((hole,index) =>{
+    hole.addEventListener("click",() =>{
+        if (activeMoles[index]) {
+            let mole = activeMoles[index]
+            mole.hits += 1
+            // NORMAL
+            if(mole.type === "normal") {
+                if (mole.hits >= mole.maxhits){
+                    console.log("mole was defeated")
+                    combo += 1
+                    comboLabel.textContent = combo
+                    hole.querySelector(".mole").classList.remove("show")
+                    delete activeMoles[index]
+                    updatecombomodifier()
+                }
+            }
+            // TOUGH
+            else if (mole.type === "tough"){
+                if(mole.hits >= mole.maxhits){
+                    console.log("tough mole defeated")
+                    hole.querySelector(".mole").classList.remove("show")
+                    combo += 1
+                    comboLabel.textContent = combo
+                    delete activeMoles[index]
+                    updatecombomodifier()
+                }
+            }
+            // TRAP
+            else if(mole.type === "trap") {
+                lives -= mole.damage
+                combo = 0
+                comboLabel.textContent = combo
+                console.log("you lost a life")
+                console.log("Lives", lives)
+                hole.querySelector(".mole").classList.remove("show")
+                delete activeMoles[index]
+                updatecombomodifier()
+            }
+            // FAST
+            else if (mole.type === "fast") {
+                timeleft += mole.timebonus
+                clearTimeout(mole.timer)
+                combo += 1
+                comboLabel.textContent = combo
+                hole.querySelector(".mole").classList.remove("show")
+                delete activeMoles[index]
+                updatecombomodifier()
+            }
+        }
+    })
+})
+
+
+// game counter //
+function settime (){
+    isGameRunning = true
+    let timer = setInterval(() => {
+        timeleft -=1 // what we will display
+        console.log("Time",timeleft)
+        if(timeleft<=0) {
+            timeleft=0
+            clearInterval(timer)
+            clearInterval(spawntimer)
+            isGameRunning = false
+            console.log("game over")
+            return
+        }
+    },2000) //ms to timer end
 }
 
-spawnMole()
-spawnMole()
-spawnMole()
+function updatecombomodifier(){
+    if (combo >= 20) {
+        combomodifier = 3
+    }
+
+    else if (combo >= 10) {
+        combomodifier = 2
+    }
+
+    else if (combo >= 5) {
+        combomodifier = 1.5
+    }
+
+    else {
+        combomodifier = 1
+    }
+}
